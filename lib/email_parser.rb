b139e9ae0c2fb3516196ab575a72b83b167acd48
Edward Williams <eddie_w38@yahoo.com>
2020-02-22 07:57:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,10 +14,10 @@ class EmailParser
   end 
 end 
 
-emails1 = ""avi@test.com", "arel@test.com""
+emails1 = "avi@test.com", "arel@test.com"
 parser1 = EmailParser.new(emails1)
 parser1.parse 
 
-emails2 = ""avi@test.com", "arel@test.com""
+emails2 = "avi@test.com", "arel@test.com"
 parser2 = EmailParser.new(emails2)
 parser2.parse
